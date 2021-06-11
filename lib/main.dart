3f9e39e595996e8bbe1c96447ca927493ae0b4dd
dizelvinable <sadr3479@gmail.com>
2021-06-11 09:22:32 +0300

    68. Step 3 - Create a Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,21 @@ class BallPage extends StatelessWidget {
         backgroundColor: Colors.blue[900],
         title: Text('Ask Me Anything'),
       ),
-      body: Container(),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
     );
   }
 }
